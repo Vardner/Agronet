afed64426742d8072e059581fff01907abff8494
Yehor Yekaterynin <yehor.yekaterynin@gmail.com>
2019-02-18 07:47:39 +0200

    Finish webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,123 +2,132 @@
 
 const path = require('path');
 const webpack = require('webpack');
-const glob = require('glob');
 const devMode = process.env.NODE_ENV !== 'production';
+
+const glob = require('glob');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const autoprefixer = require("autoprefixer");
 
-// const mix = require('laravel-mix');
+function getPlugins () {
+  const plugins = [];
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+  if (devMode) {
+    plugins.push(
+        new webpack.LoaderOptionsPlugin({
+          options: {
+            postcss: [
+              autoprefixer()
+            ]
+          }
+        })
+    );
+  }
 
-// mix.js('resources/js/app.js', 'public/js')
-//     .sass('resources/sass/app.scss', 'public/css');
+  plugins.push(
+      new CopyWebpackPlugin([
+        {
+          from: path.resolve(__dirname, 'resources/images/'),
+          to: path.resolve(__dirname, 'public/images/'),
+          toType: 'dir'
+        },
+        {
+          from: path.resolve(__dirname, 'resources/fonts/'),
+          to: path.resolve(__dirname, 'public/fonts/'),
+          toType: 'dir'
+        }
+      ])
+  );
+
+  plugins.push(
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        Vue: ['vue/dist/vue.esm.js', 'default']
+      }),
+  );
+
+  plugins.push(
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+      }),
+  );
+
+  return plugins;
+}
 
 module.exports = {
-    mode: devMode ? 'development' : 'production',
+  mode: devMode ? 'development' : 'production',
+  watch: true,
 
-    entry: [glob.sync('./resources/js/frontend/**/*.js'), glob.sync('./resources/sass/**/*.scss')],
+  entry: glob.sync('./resources/js/frontend/**/*.js').concat(['./resources/sass/app.scss']),
 
-    output: {
-        path: path.resolve(__dirname,'public/js/frontend'),
-        filename: '[name].js'
-    },
+  output: {
+    path: path.resolve(__dirname, 'public/js/frontend'),
+    filename: '[name].js'
+  },
 
-    optimization: {
-        minimizer: [
-            new UglifyJsPlugin({
-                include: path.resolve(__dirname, 'resources/js/frontend/'),
-                parallel: true,
-                cache: true,
-                uglifyOptions: {
-                    mangle: true,
-                    ie8: false,
-                },
-                sourceMap: devMode
-            })
+  optimization: {
+    noEmitOnErrors: true,
+    minimizer: [
+      new UglifyJsPlugin({
+        include: path.resolve(__dirname, 'resources/js/frontend/'),
+        parallel: true,
+        cache: true,
+        uglifyOptions: {
+          mangle: true,
+          ie8: false,
+        },
+        sourceMap: true
+      })
+    ]
+  },
+
+  watchOptions: {
+    aggregateTimeout: 100
+  },
+
+  devtool: 'source-map',
+
+  module: {
+    rules: [
+      {
+        loader: 'eslint-loader',
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        include: path.resolve(__dirname, 'resources/js/frontend/'),
+        options: {
+          configFile: 'eslint.json',
+          formatter: require('eslint-formatter-friendly'),
+          failOnError: true,
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        include: path.resolve(__dirname, 'resources/js/frontend/'),
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              '@babel/preset-env'
+            ]
+          ]
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          // "style-loader",
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader"
         ]
-    },
+      }
+    ]
+  },
 
-    watch: devMode,
-
-    watchOptions: {
-        aggregateTimeout: 100
-    },
-
-    devtool: devMode ? 'source-map' : null,
-
-    module: {
-        rules: [
-            {
-                loader: 'eslint-loader',
-                enforce: 'pre',
-                test: /\.js$/,
-                exclude: /node_modules/,
-                include: path.resolve (__dirname, 'resources/js/frontend/'),
-                options: {
-                    configFile: 'eslint.json',
-                    formatter: require('eslint-formatter-friendly'),
-                    failOnError: true,
-                }
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                include: path.resolve (__dirname, 'resources/js/frontend/'),
-                loader: 'babel-loader',
-                options: {
-                    presets: [
-                        [
-                            '@babel/preset-env'
-                        ]
-                    ]
-                }
-            },
-            {
-                test: /\.scss$/,
-                include: path.resolve (__dirname, 'resources/sass/frontend/'),
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    "css-loader",
-                    "postcss-loader",
-                    "sass-loader"
-                ]
-            }
-        ]
-    },
-
-    plugins: [
-        new webpack.NoEmitOnErrorsPlugin(),
-
-        new webpack.DefinePlugin({
-            devMode: devMode
-        }),
-
-        new webpack.ProvidePlugin({
-            $: 'jquery'
-        }),
-
-        new MiniCssExtractPlugin({
-            path: "./resources/sass/app.scss",
-            filename: "[name].css"
-        }),
-
-        new webpack.LoaderOptionsPlugin({
-            options: {
-                postcss: [
-                    autoprefixer()
-                ]
-            }
-        })
-    ],
+  plugins: getPlugins(),
 };
